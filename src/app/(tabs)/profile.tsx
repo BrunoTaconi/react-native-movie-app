a243768d71d5b5b46908f51a46b3fa5d99cc2a75
brunotaconi <brunotaconi@gmail.com>
2026-08-18 09:46:38 -0300

    feat: movie details and some adjusts

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,9 +1,13 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Ionicons name="person" size={17} color="#151312" />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
     </View>
   );
 };
